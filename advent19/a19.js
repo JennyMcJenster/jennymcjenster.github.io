d2e1c8c740738069ee9c45440014c3feb8e4b2e5
Jenny Higgs <jennymcjenster@live.co.uk>
2019-12-03 02:14:17 +0000

    Day 2!

--- advent19/a19.js
+++ advent19/a19.js
@@ -69,5 +69,114 @@ function (Input)
 	}
 	return fuel;
 }
+]},{
+	title: "1202 Program Alarm",
+	input: "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,9,1,19,1,19,5,23,1,23,5,27,2,27,10,31,1,31,9,35,1,35,5,39,1,6,39,43,2,9,43,47,1,5,47,51,2,6,51,55,1,5,55,59,2,10,59,63,1,63,6,67,2,67,6,71,2,10,71,75,1,6,75,79,2,79,9,83,1,83,5,87,1,87,9,91,1,91,9,95,1,10,95,99,1,99,13,103,2,6,103,107,1,107,5,111,1,6,111,115,1,9,115,119,1,119,9,123,2,123,10,127,1,6,127,131,2,131,13,135,1,13,135,139,1,9,139,143,1,9,143,147,1,147,13,151,1,151,9,155,1,155,13,159,1,6,159,163,1,13,163,167,1,2,167,171,1,171,13,0,99,2,0,14,0",
+	parts: [
+function (Input)
+{
+	var I = Input.split(",");
+	I[1] = "12"; I[2] = "2";
+	var IL = I.length;
+	var curpos = 0;
+	var get_io = function (p_opcode)
+	{
+		if (p_opcode+3 >= IL) return "Input too short to get i/o";
+		var vpos_a = parseInt(I[p_opcode+1]);
+		if (vpos_a >= IL) return "IO index (input 1) out of range";
+		var vpos_b = parseInt(I[p_opcode+2]);
+		if (vpos_b >= IL) return "IO index (input 2) out of range";
+		var vpos_o = parseInt(I[p_opcode+3]);
+		if (vpos_o >= IL) return "IO index (output) out of range";
+		return [parseInt(I[vpos_a]), parseInt(I[vpos_b]), vpos_o];
+	};
+	var oplist = {
+		"1": function (Pos) {
+			var IO = get_io(Pos);
+			if (typeof IO === "string") return IO;
+			I[IO[2]] = IO[0] + IO[1];
+			return 4; // move pointer by 4
+		},"2": function (Pos) {
+			var IO = get_io(Pos);
+			if (typeof IO === "string") return IO;
+			I[IO[2]] = IO[0] * IO[1];
+			return 4; // move pointer by 4
+		},"99": function (Pos) {
+			return false; // halt program
+		}
+	};
+	while (curpos < IL)
+	{
+		var opcode = I[curpos];
+		if (opcode in oplist)
+		{
+			var opres = (oplist[opcode])(curpos);
+			if (typeof opres === "string") return opres;
+			if (opres == false) return I[0]; // result
+			if (typeof opres === "number")
+			{ curpos += opres; continue; }
+		}
+		return "Unexpected stop";
+	}
+	return I.join(",");
+},
+function (Input)
+{
+	var expect = "19690720";
+	for (var noun=0; noun<100; ++noun)
+		for (var verb=0; verb<100; ++verb)
+	{
+		var I = Input.split(",");
+		I[1] = String(noun);
+		I[2] = String(verb);
+		var IL = I.length;
+		var curpos = 0;
+		var get_io = function (p_opcode)
+		{
+			if (p_opcode+3 >= IL) return "Input too short to get i/o";
+			var vpos_a = parseInt(I[p_opcode+1]);
+			if (vpos_a >= IL) return "IO index (input 1) out of range";
+			var vpos_b = parseInt(I[p_opcode+2]);
+			if (vpos_b >= IL) return "IO index (input 2) out of range";
+			var vpos_o = parseInt(I[p_opcode+3]);
+			if (vpos_o >= IL) return "IO index (output) out of range";
+			return [parseInt(I[vpos_a]), parseInt(I[vpos_b]), vpos_o];
+		};
+		var oplist = {
+			"1": function (Pos) {
+				var IO = get_io(Pos);
+				if (typeof IO === "string") return IO;
+				I[IO[2]] = IO[0] + IO[1];
+				return 4; // move pointer by 4
+			},"2": function (Pos) {
+				var IO = get_io(Pos);
+				if (typeof IO === "string") return IO;
+				I[IO[2]] = IO[0] * IO[1];
+				return 4; // move pointer by 4
+			},"99": function (Pos) {
+				return false; // halt program
+			}
+		};
+		var output = "";
+		while (curpos < IL)
+		{
+			var opcode = I[curpos];
+			if (opcode in oplist)
+			{
+				var opres = (oplist[opcode])(curpos);
+				if (typeof opres === "string") return opres;
+				if (opres == false) {
+					if (I[0] == expect)
+						return String(I[1]) + String(I[2]);
+					else break;
+				}
+				if (typeof opres === "number")
+				{ curpos += opres; continue; }
+			}
+			return "Unexpected stop";
+		}
+		continue;
+	}
+}
 ]}
 ];
